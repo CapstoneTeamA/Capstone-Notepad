fbf65d9e3f1015e5ca40d7818a06c9a6c7dc020a
David <dwagg@pdx.edu>
2017-06-17 17:20:20 -0700

    Initial Commit

---
+++ Capstone Notepad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Capstone Notepad
+//
+//  Created by PSU2 on 6/17/17.
+//  Copyright © 2017 PSU2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
